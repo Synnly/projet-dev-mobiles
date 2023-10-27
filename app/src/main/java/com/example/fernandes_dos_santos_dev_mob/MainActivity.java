802a8d51f969d623640a9e0c3583b935b29cbc25
Synnly <emanuelfernandes54@gmail.com>
2023-10-27 16:09:08 +0200

    Ajout du projet android dans la forge

---
+++ app/src/main/java/com/example/fernandes_dos_santos_dev_mob/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.fernandes_dos_santos_dev_mob;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
